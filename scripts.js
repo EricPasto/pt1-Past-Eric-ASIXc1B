35bae2a1406cfd29b151a3b24b5d6dddec6b08fc
Eric Pastó <113977250+EricPasto@users.noreply.github.com>
2023-05-05 08:43:44 +0000

    a

---
+++ scripts.js
@@ -0,0 +1,66 @@
+// Función para actualizar el reloj en la página web
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+
+    hours = checkTime(hours);
+    minutes = checkTime(minutes);
+    seconds = checkTime(seconds);
+
+    document.getElementById("clock").innerHTML = hours + ":" + minutes + ":" + seconds;
+
+    setTimeout(updateClock, 1000);
+}
+
+// Función auxiliar para agregar un cero antes de los números menores a 10
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+// Variables para el temporizador
+var timer;
+var secondsLeft;
+
+// Función para iniciar el temporizador
+function startTimer() {
+    var inputSeconds = parseInt(prompt("Ingrese la cantidad de segundos para el temporizador:", "60"));
+    if (isNaN(inputSeconds) || inputSeconds < 1) {
+        alert("Ingrese un número entero positivo mayor que cero.");
+        return;
+    }
+    secondsLeft = inputSeconds;
+    updateTimer();
+    timer = setInterval(updateTimer, 1000);
+}
+
+// Función para detener el temporizador
+function stopTimer() {
+    clearInterval(timer);
+}
+
+// Función para restablecer el temporizador
+function resetTimer() {
+    secondsLeft = 0;
+    updateTimer();
+    clearInterval(timer);
+}
+
+// Función para actualizar el temporizador en la página web
+function updateTimer() {
+    document.getElementById("timer").innerHTML = secondsLeft + " segundos";
+    secondsLeft--;
+    if (secondsLeft < 0) {
+        clearInterval(timer);
+        alert("¡Tiempo terminado!");
+    }
+}
+
+// Función para seleccionar un nombre aleatorio de la lista de nombres ingresados
+function pickRandomName() {
+    var names = document.getElementById("names").value.split(",");
+    if (names.length < 2) {
